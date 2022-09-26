db36716bc33be9d2fd5a030fc206aa0e23f9fe02
Fabio <fabio.bernasconi8@gmail.com>
2022-09-26 20:37:52 +1300

    demo

--- code/codeAnimator.py
+++ code/codeAnimator.py
@@ -1,12 +1,14 @@
-import pygame
-screenSizeMult = 100
-screenWidth = 16 * screenSizeMult
-screenHeight = 9 * screenSizeMult
-screenName = 'Code Animator'
-pygame.init()
-screen=pygame.display.set_mode((screenWidth,screenHeight))
-pygame.display.set_caption(screenName)
+#inputs
+text = [ #Demo from lines 25 to 27
+    '¤cimport ¤lpygame',
+    '¤vscreenSizeMult ¤w= ¤n100',
+    '¤vscreenWidth ¤w= ¤n16 ¤w* ¤vscreenSizeMult',
+]
+captionText = 'Playing audio using python'
+fileName = captionText
 
+
+#set colors
 colors = {
     'background': (30,30,30),
     'lineNos': (133,133,133),
@@ -18,30 +20,32 @@ colors = {
     'c': (217,110,192), #command pink/purple
     'b': (255,224,0),   #bracket yellow
 }
+
+#pygame and screen setup
+import pygame
+screenSizeMult = 100
+screenWidth = 16 * screenSizeMult
+screenHeight = 9 * screenSizeMult
+screenName = 'Code Animator'
+pygame.init()
+screen=pygame.display.set_mode((screenWidth,screenHeight))
+pygame.display.set_caption(screenName)
 screen.fill(colors['background'])
 
 pixelSize = 8
 
-fontSize = 5*pixelSize
-pixelFont = pygame.font.Font('customPixelFont.ttf',fontSize)
-
+topBarFontSize = 5*pixelSize
+topBarFont = pygame.font.Font('customPixelFont.ttf',topBarFontSize)
 topBarButtons = pygame.transform.scale(pygame.image.load('windowButtons.bmp'),(pygame.image.load('windowButtons.bmp').get_width()*pixelSize, pygame.image.load('windowButtons.bmp').get_height()*pixelSize))
 topBarHeight = topBarButtons.get_height() + 2*pixelSize
 pygame.draw.rect(screen, (0,0,0), pygame.Rect(0,0,screenWidth,topBarHeight))
 screen.blit(topBarButtons,(screenWidth - (topBarButtons.get_width() + pixelSize), pixelSize))
-captionText = 'Code - Play Tone.py'
-caption = pixelFont.render(captionText, False, (255,255,255))
+caption = topBarFont.render(captionText, False, (255,255,255))
 captionX = (screenWidth - caption.get_width()) / 2
 screen.blit(caption, (captionX, 0))
 
 fontSize = 8*pixelSize
 pixelFont = pygame.font.Font('customPixelFont.ttf',fontSize)
-
-text = [
-    '¤cimport ¤lmusic',
-    '',
-    '¤lmusic¤w.¤lpitch¤b(¤n600¤w,¤n540¤b)',
-]
 
 color = colors['w']
 splitText = []
@@ -53,10 +57,11 @@ for iLine in range(len(text)):
     line = text[iLine]
     iChar = 0
     curX = pixelSize*2 + numWidth*2
-    curY += pixelSize + fontSize
-    num = pixelFont.render(str(iLine),False,colors['n'])
+    curY += pixelSize*2 + fontSize
+
+    num = pixelFont.render(str(iLine+1),False,colors['lineNos'])
     numRect = num.get_rect(right = pixelSize + numWidth*2, y = curY)
-    screen.blit(pixelFont.render(str(iLine),False,colors['n']), numRect)
+    screen.blit(num, numRect)
     curX += pixelSize*2
 
     while iChar < len(line):
@@ -69,14 +74,6 @@ for iLine in range(len(text)):
             screen.blit(let, (curX, curY))
             curX += let.get_width()
             pygame.display.update()
-            pygame.image.save(screen, f'export/{captionText} - {charNo}.bmp')
+            pygame.image.save(screen, f'export/{captionText} - {charNo:02d}.bmp')
             charNo += 1
             iChar += 1
-            
-while True:
-
-    for event in pygame.event.get():
-
-        if event.type==pygame.QUIT:
-            pygame.quit()
-            quit()
